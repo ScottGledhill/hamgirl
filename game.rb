46b63fba92d41f5b50af4ae06792976f402f7161
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-10-05 13:23:06 +0100

    counter added

--- game.rb
+++ game.rb
@@ -10,9 +10,10 @@ class GameWindow < Gosu::Window
   def initialize
     super 640, 480
     self.caption = "HAMGIRL"
-    @background_image = Gosu::Image.new("media/space2.jpg", :tileable => true)
+    @background_image = Gosu::Image.new("media/space.jpg", :tileable => true)
     @player = Player.new
     @player.warp(320, 240)
+    @font = Gosu::Font.new(20)
     @ham_anim = Gosu::Image::load_tiles("media/ham.png", 25, 25)
     @hams = Array.new
   end
@@ -40,6 +41,7 @@ class GameWindow < Gosu::Window
     @background_image.draw(0, 0, ZOrder::Background)
     @player.draw
     @hams.each { |ham| ham.draw }
+    @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xff_ffff00)
   end
 
   def button_down(id)
